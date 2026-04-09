e695d38faf4e177dad76ac71ad32d120852b0f5b
mohit8182098-collab <mohit8182098@gmail.com>
2026-04-09 23:46:34 +0530

    Add files via upload

---
+++ First.java
@@ -0,0 +1,10 @@
+// class Main {
+//     public static void main(String[] args) {
+//         System.out.println("Hello Java!");
+//     }
+// }
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
